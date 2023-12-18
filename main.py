2e06667135d9832ac1fb7146415d8a53631209d0
Michal Wiorek <wiorekster@gmail.com>
2023-12-18 22:24:59 +0100

    Get headers of table

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ soup = BeautifulSoup(site_text, "html.parser")
 paginator = soup.select(selector="div.pagination.csr-gridpage__pagination a")
 total_pages = int(paginator[-2].text)
 
+
+table = soup.select(selector="table.data-table tbody tr")
+headers = [element.select_one(selector="span.data-table__title").text[:-1] for element in table[0]]
