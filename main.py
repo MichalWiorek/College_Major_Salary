ed2f35988d7ed58c016a57ce40e5ce228c0e7dc7
Michal Wiorek <wiorekster@gmail.com>
2023-12-18 22:25:35 +0100

    Get values of table

--- main.py
+++ main.py
@@ -16,3 +16,19 @@ total_pages = int(paginator[-2].text)
 
 table = soup.select(selector="table.data-table tbody tr")
 headers = [element.select_one(selector="span.data-table__title").text[:-1] for element in table[0]]
+
+all_values = []
+for i in range(1, total_pages+1):
+    response = requests.get(url=url+f"/page/{i}")
+    response.raise_for_status()
+    site_text = response.text
+    soup = BeautifulSoup(site_text, "html.parser")
+    table = soup.select(selector="table.data-table tbody tr")
+    for row in table:
+        values = []
+        for element in row:
+            value = element.select_one(selector="span.data-table__value")
+            values.append(value.text)
+        all_values.append(values)
+        print(values)
+
